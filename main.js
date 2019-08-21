daa6b8f7d8416aca4b962b0969611cd65e7e337a
John Adams <johnadams@Johns-MacBook-Pro.local>
2019-08-21 16:46:13 -0600

    Add script to HTML and add function for updating the range

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 var minRangeInput = document.getElementById('min-range');
 var maxRangeInput = document.getElementById('max-range');
-var updateRangeButton = document.getElementById('range-update-button')
+var updateRangeButton = document.getElementById('range-update-button');
+var currentRangeMin = document.getElementById('current-min');
+var currentRangeMax = document.getElementById('current-max');
+
+function updateRange() {
+  currentRangeMin.innerhtml = minRangeInput.value;
+  currentRangeMax.innerhtml = maxRangeInput.value;
+}
+
+updateRangeButton.addEventListener('click', updateRange);

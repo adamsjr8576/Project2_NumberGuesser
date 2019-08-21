c88fb5b8ff479df967411bbbe55a51d14627b869
John Adams <johnadams@Johns-MacBook-Pro.local>
2019-08-21 17:33:12 -0600

    hack fix for updating numbers on form and then code to get random number in range and store in a variable

--- main.js
+++ main.js
@@ -3,10 +3,19 @@ var maxRangeInput = document.getElementById('max-range');
 var updateRangeButton = document.getElementById('range-update-button');
 var currentRangeMin = document.getElementById('current-min');
 var currentRangeMax = document.getElementById('current-max');
-
-function updateRange() {
-  currentRangeMin.innerhtml = minRangeInput.value;
-  currentRangeMax.innerhtml = maxRangeInput.value;
-}
+var randomNum = getRandomNum();
 
 updateRangeButton.addEventListener('click', updateRange);
+updateRangeButton.addEventListener('click', getRandomNum);
+
+function updateRange() {
+  event.preventDefault(event);
+  currentRangeMin.innerText = minRangeInput.value;
+  currentRangeMax.innerText = maxRangeInput.value;
+};
+
+function getRandomNum() {
+  var min = Math.ceil(parseInt(minRangeInput.value));
+  var max = Math.floor(parseInt(maxRangeInput.value));
+  return Math.floor(Math.random() * (max - min)) + min;
+}
